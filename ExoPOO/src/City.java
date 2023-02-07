4a86bd9c46c030f68e64176c261285588a9adc7c
Raphael Aldaz <raphael.aldaz@gmail.com>
2023-02-07 16:36:41 +0100

    Exercie 1.2

--- ExoPOO/src/City.java
+++ ExoPOO/src/City.java
@@ -6,9 +6,40 @@ public class City {
 	public int numberOfResident;
 	
 	public City (String name, String country, int numberOfResident) {
+		setName( name);
+		setCountry(country);
+		setNumberOfResident(numberOfResident);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName( String name) {
 		this.name = name;
-		this.country=country;
-		this.numberOfResident = numberOfResident;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		 this.country= country;
+		
+	}
+	
+	public int getNumberOfResident() {
+		return numberOfResident;
+	}
+	
+	public void setNumberOfResident(int numberOfResident) {
+		if(numberOfResident >= 0) {
+			this.numberOfResident = numberOfResident;
+		} else {
+			System.out.println("Il ne peux pas y avoir un nombre negatif d'habitant");
+		}
+		
+			
+			
+		
 	}
 	
 	public  void display() {
